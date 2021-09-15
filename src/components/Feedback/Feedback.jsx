2549b5347eaf0f14a6abbc1c055e11d2ee6ce60d
MarinaMuntian <dp060491smv@gmail.com>
2021-09-16 00:55:55 +0300

    add statistics

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -8,17 +8,41 @@ class Feedback extends React.Component {
     bad: 0,
   };
 
+  addGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  addNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+
+  addBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Please leave feedback</h1>
-        <button type="button">Good</button>
-        <button type="button">Neutral</button>
-        <button type="button">Bad</button>
+        <button type="button" onClick={this.addGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.addNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.addBad}>
+          Bad
+        </button>
         <h2>Statistics</h2>
-        <p>Good:</p>
-        <p>Neutral:</p>
-        <p>Bad:</p>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
       </div>
     );
   }
